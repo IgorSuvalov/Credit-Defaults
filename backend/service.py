3ef7716eba9ea33ef26047044d77407b37165c39
IgorSuvalov <igorshuvalov23@gmail.com>
2025-09-29 20:24:22 +0100

    dockerised frontend, docker compose added

--- backend/service.py
+++ backend/service.py
@@ -19,12 +19,12 @@ class ClientData(BaseModel):
 
 app = FastAPI()
 
-origins = ["http://localhost:5173", "http://localhost:8004"]
+origins = ["http://localhost:5173", "http://localhost:8004", "http://127.0.0.1:5173"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
